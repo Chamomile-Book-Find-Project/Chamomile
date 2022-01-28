84dcc870115ed1f50e77230647beb76007c36e08
JoeCP17 <at365@naver.com>
2022-01-28 17:27:24 +0900

    Docker + nginx + back

--- back-end/app.py
+++ back-end/app.py
@@ -1,10 +1,12 @@
-from flask import Flask 
+from flask import Flask, render_template 
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'hello'
+def book_page():
+    return render_template("Frontend.html")
      
 
 # flask 예시 -> 띄울때는 flask run으로 실행하면됨  *주의* 실행할 때, 해당 파일내까지 접근하고 해야한다.
+
+# render_template를 활용하여 html 연결해보고자 시도함 근데안됨 (수정하셔도 됩니다.)
