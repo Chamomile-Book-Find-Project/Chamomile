960b196d13f1b7736e8b4849c9b0b28760a697c6
JoeCP17 <at365@naver.com>
2022-02-04 15:09:03 +0900

    change

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,8 @@ from flask import Flask
 from pymongo import MongoClient 
 
 
-client = MongoClient('DATABASE LINK')
+client = MongoClient('mongodb://')
 db = client.Book_data_DB 
+collections = db.Book_data_DB
 
 app = Flask(__name__)
