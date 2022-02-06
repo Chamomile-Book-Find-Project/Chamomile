f4309aa86a3fd2060f1830d4cd5d37c53aeec626
JoeCP17 <at365@naver.com>
2022-02-07 02:11:42 +0900

    docker-compose 문제 수정진행중

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
-from flask import Flask 
+from flask import Flask, render_template
 from pymongo import MongoClient 
+from elasticsearch import Elasticsearch 
 
+# Data Base 
+client = MongoClient('0.0.0.0:27017')
+db = client.Book_data_DB # 데이터 베이스 명 
+# collection = db. 컬렉션 명 작성 
+#results = collection.find()
+
+
+es = Elasticsearch()
 app = Flask('__name__')
 
 @app.route('/')
-def index():
-    return 'Hello world!'
+def main_page():
+    return 'hello world'
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port = "5001", debug=True)
