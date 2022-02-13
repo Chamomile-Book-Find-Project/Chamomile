1fdb09883c5639c0ceb2b076805c4fc3dc91909f
JoeCP17 <at365@naver.com>
2022-02-14 00:10:12 +0900

    ElasticSearch  + Mongo 연동작업 성공 + api 수정

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,3 @@
-from sqlite3 import connect
 import elasticsearch
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
@@ -6,12 +5,8 @@ from flask_cors import CORS
 import os 
 from werkzeug.utils import secure_filename # 파일 안정성 검사 
 from elasticsearch import Elasticsearch, helpers 
-from elasticsearch.helpers import bulk, parallel_bulk
-from tqdm import tqdm 
 import pandas as pd 
 import json 
-
-
 
 
 app = Flask('__name__')
@@ -34,7 +29,6 @@ data.reset_index(inplace=True)
 data_dict = data.to_dict ("records")
 collection.insert_many(data_dict)
 
-
 # # elastic search connect
 es = Elasticsearch(
     hosts=['http://elasticsearch:9200'],
@@ -46,54 +40,65 @@ if not es.ping():
 else: 
     print('connection successful')
 
-actions = []
-for data_index in tqdm(results, total= len(list(results))):
-    data_index.pop('_id')
-    action = {
-        "index": {
-                    "Category" : data_index['Category'],
-                    "Title" : data_index['Title'],
-                    "Writer" : data_index['Writer'],
-                    "Book_made": data_index['Book_made'],
-                    "Sell_price" : data_index['Sell_price'],
-                    "Image_uri" : data_index['Image_uri']
+# elastic search & mongodb 
+class My_MongoDB() :
+    def __init__(self) :
+        self.client = MongoClient("mongodb://root:chamomile123@mongodb:27017/")
 
-                }
-    }
-    actions.append(action)
-    actions.append(data_index)
+    def Get_Data(self, db, collection) :
+        return self.client[db][collection].find({})
+            
+    def __del__(self) :
+        self.client.close()
 
-request_body = { 
-    "settings" : { 
-        "number_of_shards": 3,
-        "number_of_replicas": 1
-    }
-}
-
-es.indices.create(index='Book',body = request_body, ignore = 400)
-res = es.bulk(index = 'Book', body = actions, refresh = True)
-
-
-@app.route('/', methods=['POST'])
-def main_page():
-    # 이미지 업로드 및 저장 
-    if request.method == 'POST':
-        image_data = request.files['file']  # 이미지 파일을 불러와서 images폴더에 저장
-        image_data.save(os.path.join(app.config['UPLOAD_FOLDER'], image_data.filename))
-
-        filename = secure_filename(image_data.filename)  # 파일 안정성 검사 
+class My_Elasticsearch() :
+    def __init__(self) :
+        self.es = Elasticsearch(
+        hosts=['http://elasticsearch:9200'],
+        http_auth=('elastic','chamomile123')
+        )
         
-    return jsonify({'success': True, 'file':'Received', 'name': filename })   # file : Received : 어떤 내용인지 찾아봐야함 
+    def Search(self, _index, _body) :
+        return self.es.search(index=_index, body={_body})
+    
+    def Insert(self, _index, _data) :
+        with open('mapping.json', 'r') as f: 
+            mapping = json.load(f)
 
+        self.es.indices.create(index = _index, body = mapping)
+        helpers.bulk(self.es, _data, index = _index)
+
+
+def es_import() :
+    mongodb = My_MongoDB()
+    mongo_data = pd.DataFrame(mongodb.Get_Data("BookDB", "Book_data"))
+    del(mongo_data['_id'])
+    data = mongo_data.to_dict('records')
+    es = My_Elasticsearch()
+    es.Insert("book_idx",data)
+
+@app.route('/', methods=['GET'])
+def main():
+    return 'Backend-server Connect'
+
+@app.route('/data/inout', methods=['POST'])
+def data():
+    return 'Data Server'
 
 # 단순 데이터 베이스, 데이터 확인 부분 
-@app.route('/mongo/check', methods=['GET'])
-def mongoTest():
+@app.route('/db/mongo', methods=['GET'])
+def mongo():
     global client, db, collection,results
     
     return render_template('test.html',data=results)
 
 # 이미지 업로드 api 부분은 어느정도 된듯 ,  open api + elastic search + mongodb 이렇게 세가지 연결해줘야함 
+
+@app.route('/db/elastic')
+def elastic():
+    global My_MongoDB, My_Elasticsearch, es_import
+    es_import()
+    return 'Data Import!'
 
 
 if __name__ == '__main__':
