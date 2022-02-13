3f1621e4009df675d5d2e9dfca106596337170cf
JoeCP17 <at365@naver.com>
2022-02-14 03:10:54 +0900

    open api 연동

--- backend/app.py
+++ backend/app.py
@@ -5,9 +5,13 @@ from flask_cors import CORS
 import os 
 from werkzeug.utils import secure_filename # 파일 안정성 검사 
 from elasticsearch import Elasticsearch, helpers 
+
+import requests 
+import uuid 
+import time 
 import pandas as pd 
 import json 
-
+import base64
 
 app = Flask('__name__')
 
@@ -23,9 +27,10 @@ collection = db.Book_data
 results = collection.find({})
 
 # import csv in Database (MongoDB & ElasticSearch)
-# mongo DB 
+
+# mongo DB  Data import 
 data = pd.read_csv('DB_txt.csv')
-data.reset_index(inplace=True)
+data.reset_index(inplace=False)
 data_dict = data.to_dict ("records")
 collection.insert_many(data_dict)
 
@@ -35,12 +40,15 @@ es = Elasticsearch(
     http_auth=('elastic','chamomile123')
 )
 
+# check elasitc connect 
 if not es.ping():
     print('connection failed')
 else: 
     print('connection successful')
 
-# elastic search & mongodb 
+# elastic search & mongodb connect
+
+# Get mongoDB data
 class My_MongoDB() :
     def __init__(self) :
         self.client = MongoClient("mongodb://root:chamomile123@mongodb:27017/")
@@ -51,6 +59,7 @@ class My_MongoDB() :
     def __del__(self) :
         self.client.close()
 
+# Create Elasticsearch index & import  
 class My_Elasticsearch() :
     def __init__(self) :
         self.es = Elasticsearch(
@@ -77,24 +86,67 @@ def es_import() :
     es = My_Elasticsearch()
     es.Insert("book_idx",data)
 
+
 @app.route('/', methods=['GET'])
 def main():
     return 'Backend-server Connect'
 
-@app.route('/data/inout', methods=['POST'])
+
+@app.route('/data/upload', methods=['POST'])
 def data():
-    return 'Data Server'
+    if request.method == 'POST' : 
+        image_data = request.files['file']  # 이미지 파일을 불러와서 images폴더에 저장        
+        filename = secure_filename(image_data.filename)  # 파일 안정성 검사 
+        image_data.save(os.path.join(app.config['UPLOAD_FOLDER'], image_data.filename)) #검사 이후, 폴더에 저장 
+        
+        folder_list = os.listdir('./images')
+        
+    return "file_list : {}".format(folder_list) 
+
+
+@app.route('/data/upload/search')
+def search():
+    # 이부분에 api 내용 옮겨담기 
+    api_url = 'https://0k7t1bylkc.apigw.ntruss.com/' \
+              'custom/v1/14174/5de3c9ce2fd1434efa8cf011811303cf21bbe4941c8e28cd1ff433873ed56d6c/general'
+    secret_key = 'ZGdXQWlqVld1ZFdkWXJCYlJFYnloc0NaRGRkaU5UTHQ='
+    image_file = './images'
+    with open(image_file,'rb') as f:
+      file_data = f.read()
+
+    # api에 요청할 정보
+    request_json = {
+        'images': [
+            {
+                'format': ['jpg','png'],
+                'name': 'demo',
+                'data': base64.b64encode(file_data).decode()
+            }
+        ],
+        'requestId': str(uuid.uuid4()),
+        'version': 'V2',
+        'timestamp': int(round(time.time() * 1000))
+    }
+
+    payload = json.dumps(request_json).encode('UTF-8')
+    headers = {
+        'X-OCR-SECRET': secret_key,
+        'Content-Type': 'application/json'
+    }
+
+    response = requests.request("POST", api_url, headers=headers, data=payload)
+
+    return response.text
 
 # 단순 데이터 베이스, 데이터 확인 부분 
-@app.route('/db/mongo', methods=['GET'])
+@app.route('/data/mongo', methods=['GET'])
 def mongo():
     global client, db, collection,results
     
     return render_template('test.html',data=results)
 
-# 이미지 업로드 api 부분은 어느정도 된듯 ,  open api + elastic search + mongodb 이렇게 세가지 연결해줘야함 
 
-@app.route('/db/elastic')
+@app.route('/data/elastic')
 def elastic():
     global My_MongoDB, My_Elasticsearch, es_import
     es_import()
