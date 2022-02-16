29a9d5359af7ac5ee1a49db8797684174fdca3b2
juwon5272 <jackson5272@naver.com>
2022-02-16 15:40:21 +0900

    글씨 넣고 순서바꾸기 시도

--- frontend/src/components/Book.jsx
+++ frontend/src/components/Book.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Button.css";
+import "./BookParsing.css";
 
 //화면에 보여져야 하는 값들 정리해서 넘겨주는 것
 
@@ -8,18 +9,18 @@ function Book({ Category, Title, Writer, Bookmade, Sellprice, ImageUri }) {
   console.log(ImageUri);
   return (
     <div classname="Book__data">
-      <img classname="Book__Image__uri" src={ImageUri} alt="책이미지" float="left" />
+      <img classname="Book__Image__uri" src={ImageUri} alt="책이미지" float="left" height="300px" margin-top="40px" />
       <div
         className="miniBox"
         style={{
           position: "relative",
         }}
       >
-        <h1 classname="Book__Category">{Category}</h1>
-        <h2 classname="Book__title">{Title}</h2>
-        <h3 classname="Book__writer">{Writer}</h3>
-        <h4 classname="Book__made">{Bookmade}</h4>
-        <h5 classname="Book__Sell__price">{Sellprice}</h5>
+        Category : <h1 classname="Book__Category">{Category}</h1>
+        Title : <h2 classname="Book__title">{Title}</h2>
+        Writer : <h3 classname="Book__writer">{Writer}</h3>
+        Publisher : <h4 classname="Book__made">{Bookmade}</h4>
+        Price : <h5 classname="Book__Sell__price">{Sellprice}</h5>
       </div>
     </div>
   );
