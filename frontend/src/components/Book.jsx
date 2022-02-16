6510b61a3205a1f9331dfecc9db1f37d9215bae6
juwon5272 <jackson5272@naver.com>
2022-02-16 14:46:53 +0900

    결과창 수정

--- frontend/src/components/Book.jsx
+++ frontend/src/components/Book.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import "./Button.css";
 
 //화면에 보여져야 하는 값들 정리해서 넘겨주는 것
 
-function Book({Category,Title, Writer, Bookmade, Sellprice, ImageUri}){
-    console.log(ImageUri)
-    return(
-        <div classname="Book__data">
-            <h1 classname="Book__Category">{Category}</h1>
-            <h2 classname="Book__title">{Title}</h2>
-            <h3 classname="Book__writer">{Writer}</h3>
-            <h4 classname="Book__made">{Bookmade}</h4>
-            <h5 classname="Book__Sell__price">{Sellprice}</h5>
-            <img classname="Book__Image__uri" src={ImageUri} alt='책이미지'/>
-        </div>
-    );
+function Book({ Category, Title, Writer, Bookmade, Sellprice, ImageUri }) {
+  console.log(ImageUri);
+  return (
+    <div classname="Book__data">
+      <img classname="Book__Image__uri" src={ImageUri} alt="책이미지" float="left" />
+      <div
+        className="miniBox"
+        style={{
+          position: "relative",
+        }}
+      >
+        <h1 classname="Book__Category">{Category}</h1>
+        <h2 classname="Book__title">{Title}</h2>
+        <h3 classname="Book__writer">{Writer}</h3>
+        <h4 classname="Book__made">{Bookmade}</h4>
+        <h5 classname="Book__Sell__price">{Sellprice}</h5>
+      </div>
+    </div>
+  );
 }
-Book.propTypes ={
-    Category:PropTypes.string.isRequired,
-    Title:PropTypes.string.isRequired,
-    Writer:PropTypes.string.isRequired,
-    Bookmade:PropTypes.string.isRequired,
-    Sellprice:PropTypes.string.isRequired,
-    ImageUri:PropTypes.string.isRequired,
+Book.propTypes = {
+  Category: PropTypes.string.isRequired,
+  Title: PropTypes.string.isRequired,
+  Writer: PropTypes.string.isRequired,
+  Bookmade: PropTypes.string.isRequired,
+  Sellprice: PropTypes.string.isRequired,
+  ImageUri: PropTypes.string.isRequired,
 };
 
 export default Book;
