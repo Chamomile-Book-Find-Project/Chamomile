b79b0a9c75085a0a669fddc3acb5ee6d4761c846
juwon5272 <jackson5272@naver.com>
2022-02-18 12:58:26 +0900

    버튼 이미지로 변경(이미지는 아직 구림)

--- frontend/src/components/BookParsing.jsx
+++ frontend/src/components/BookParsing.jsx
@@ -4,6 +4,10 @@ import useAsync from "./useAsync";
 import Book from "./Book";
 import "./Button.css";
 import "./BookParsing.css";
+import Button from "./images/Button.png";
+import CameraIcon from "./images/cameraIcon.png";
+
+
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
 // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
 async function getBooks() {
@@ -15,8 +19,18 @@ function BookParsing2() {
   const { loading, data: books, error } = state; // state.data 를 books 키워드로 조회
   const [effect, setEffect] = useState("mount1");
   if (loading) return <div>로딩중..</div>;
-  if (error) return <div>에러가 발생했습니다</div>;
-  if (!books) return <button onClick={refetch}>불러오기</button>;
+  if (error) return (
+    <div className="box2">
+      <div>에러가 발생했습니다</div>
+<div className="miniImg">
+        <div>에러가 발생했습니다</div>
+      </div>
+      <div className="miniBox1">
+        <div>에러가 발생했습니다</div>
+      </div>
+    </div>
+  );
+  if (!books) return <div onClick={refetch}><img src={Button} height="80px" cursor= "pointer"></img></div>;
   return (
     <>
       <div className="books">
