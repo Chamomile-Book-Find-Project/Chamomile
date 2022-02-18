5a5eaaf26302793d4dcc7650b250e8cc66786b40
juwon5272 <jackson5272@naver.com>
2022-02-18 21:08:34 +0900

    회색 박스, 버트 바꿈

--- frontend/src/components/BookParsing.jsx
+++ frontend/src/components/BookParsing.jsx
@@ -4,9 +4,8 @@ import useAsync from "./useAsync";
 import Book from "./Book";
 import "./Button.css";
 import "./BookParsing.css";
-import Button from "./images/Button.png";
+import Search from "./images/Search.png";
 import CameraIcon from "./images/cameraIcon.png";
-
 
 // useAsync 에서는 Promise 의 결과를 바로 data 에 담기 때문에,
 // 요청을 한 이후 response 에서 data 추출하여 반환하는 함수를 따로 만들었습니다.
@@ -19,35 +18,30 @@ function BookParsing2() {
   const { loading, data: books, error } = state; // state.data 를 books 키워드로 조회
   const [effect, setEffect] = useState("mount1");
   if (loading) return <div>로딩중..</div>;
-  if (error) return (
-    <div className="box2">
-      <div>에러가 발생했습니다</div>
-<div className="miniImg">
-        <div>에러가 발생했습니다</div>
+  if (error) return <div>에러가 발생했습니다</div>;
+  if (!books)
+    return (
+      <div onClick={refetch}>
+        <img src={Search} height="80px" cursor="pointer"></img>
       </div>
-      <div className="miniBox1">
-        <div>에러가 발생했습니다</div>
-      </div>
-    </div>
-  );
-  if (!books) return <div onClick={refetch}><img src={Button} height="80px" cursor= "pointer"></img></div>;
+    );
   return (
     <>
       <div className="books">
         <div className={`box-wrap ${effect}`}>
           {/* <div className="box2"> */}
-            <ul>
-              {books.result.map((book) => (
-                <Book
-                  Category={book.Category}
-                  Title={book.Title}
-                  Writer={book.Writer}
-                  Bookmade={book.Bookmade}
-                  Sellprice={book.Sellprice}
-                  ImageUri={book.ImageUri}
-                />
-              ))}
-            </ul>
+          <ul>
+            {books.result.map((book) => (
+              <Book
+                Category={book.Category}
+                Title={book.Title}
+                Writer={book.Writer}
+                Bookmade={book.Bookmade}
+                Sellprice={book.Sellprice}
+                ImageUri={book.ImageUri}
+              />
+            ))}
+          </ul>
           {/* </div> */}
         </div>
       </div>
